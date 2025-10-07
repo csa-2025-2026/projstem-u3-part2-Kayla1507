67f4e8f250c3ebf35e0ef46dde3a340c5bd85c37
per5 <klapham007@gmail.com>
2025-10-07 21:46:19 +0000

    5

--- Main.java
+++ Main.java
@@ -6,6 +6,66 @@ public class Main
   {
     Scanner scan = new Scanner(System.in);  // input from the keyboard
     /* ============= BEGIN SOLUTION ================================ */
+    System.out.println("Enter in a number");
+    double target = 12.345;
+    double user_input = scan.nextDouble();
+
+    if(user_input == target)
+    {
+      System.out.println("yes");
+
+    }
+
+    int user_int1 = scan.nextInt();
+    int user_int2 = scan.nextInt();
+    boolean twice = user_int1*2 == user_int2;
+
+    if(twice == true)
+    {
+      System.out.println("yes");
+
+    }
+
+    int divide = scan.nextInt();
+    int d2 = divide % 2;
+    int d3 = divide % 3;
+
+    if(d2 == 0)
+    {
+      System.out.println("2");
+
+    }
+
+     if(d3 == 0)
+    {
+      System.out.println("3");
+
+    }
+
+      if(d3 == 0 || d2 == 0);
+    {
+      System.out.println("23");
+
+    }
+
+    int userEvem = scan.nextInt();
+    int even = userEvem % 2;
+
+    if(even == 0)
+    {
+      System.out.println("even");
+
+    }
+
+    else
+    {
+            System.out.println("odd");
+
+    }
+
+
+    
+
 
 
 
